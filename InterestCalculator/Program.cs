389c078138dcef98d8f4c1d7bd092859d995d8c0
Mahesh Gohil <110255508+maheshgohil2022@users.noreply.github.com>
2022-07-29 15:53:21 -0400

    1.0

--- InterestCalculator/Program.cs
+++ InterestCalculator/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Inputs of the program
+
+Console.WriteLine("Please Enter The Following Information: ");
+Console.WriteLine("Principle Amount: ");
+String strPrinciple = Console.Read();
+Console.WriteLine("Interest Rate (in%): ");
+Console.WriteLine("Duration of payment: ");
+Console.WriteLine("Enter 1 for Weekly\n2 for Bi-Weekly\n3 for Monthly: ");
+Console.WriteLine("Type of Interest :\n1 for Simple\n2 for compound.");
